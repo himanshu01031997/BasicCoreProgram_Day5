ace7246abdb011f2bb6c8f1db3ba6f0363df89c4
himanshu01031997 <hs010397@gmail.com>
2022-10-07 07:44:11 +0530

    add:added welcome message

---
+++ BasicCoreProgram/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BasicCoreProgram
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to Basic Core Program!");
+        }
+    }
+}
