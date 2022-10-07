07fed8f5d89ff90d3f012e59aa88489a750dad90
himanshu01031997 <hs010397@gmail.com>
2022-10-07 21:36:06 +0530

    add:power of two

---
+++ BasicCoreProgram/PowerOfTwo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public static class PowerOfTwo
+    {
+        public const int num = 2;
+
+        public static void CheckPower()
+        {
+            int p;
+            Console.WriteLine("check the  power of two");
+            Console.WriteLine("enter power");
+            p = Convert.ToInt32(Console.ReadLine());
+            for (int power = 0; power <= 31; power++)
+            {
+                Console.WriteLine("{0}^{1} = {2:N0} ", num, p, (double)Math.Pow(num, p));
+                Console.ReadLine();
+            }
+        }
+    }
+
+}   
+            
+        
--- BasicCoreProgram/Program.cs
+++ BasicCoreProgram/Program.cs
@@ -5,39 +5,42 @@
         static void Main(string[] args)
         {
             // Console.WriteLine("welcome to Basic Core Program!");
-           
-                Console.WriteLine("Please enter your choice");
-                Console.WriteLine("1.FlipCoin\n2.Leapyear\n3.HarmonicNo\n4.EvenOrOdd\n5.CheckAlphabate\n6.SwapNo\n7.LargestNo\n8.Quotient and remainder");
-                int choice = Convert.ToInt32(Console.ReadLine());
-                switch (choice)
-                {
-                    case 1:
-                        FlipCoin.CalculatePercent();
-                        break;
-                    case 2:
-                        LeapYear.CheckYear();
-                       break;
-                    case 3:
-                        HarmonicNo.CalculateHarmonic();
-                       break;
-                    case 4:
-                       EvenOdd.CheckEvenOdd();
-                       break;
-                    case 5:
-                       VowelOrConsonent.CheckAlphabate();
-                       break;
-                    case 6:
-                       SwapNo.SwappingTwoNo();
-                       break;
-                    case 7:
-                       LargestNo.FindLargest();
+
+            Console.WriteLine("Please enter your choice");
+            Console.WriteLine("1.FlipCoin\n2.Leapyear\n3.HarmonicNo\n4.EvenOrOdd\n5.CheckAlphabate\n6.SwapNo\n7.LargestNo\n8.Quotient and remainder\n9.Power of two");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    FlipCoin.CalculatePercent();
                     break;
-                    case 8:
-                       ComputeQuotient.calcualte();
+                case 2:
+                    LeapYear.CheckYear();
                     break;
-                    default:
-                       break;
-                }
+                case 3:
+                    HarmonicNo.CalculateHarmonic();
+                    break;
+                case 4:
+                    EvenOdd.CheckEvenOdd();
+                    break;
+                case 5:
+                    VowelOrConsonent.CheckAlphabate();
+                    break;
+                case 6:
+                    SwapNo.SwappingTwoNo();
+                    break;
+                case 7:
+                    LargestNo.FindLargest();
+                    break;
+                case 8:
+                    ComputeQuotient.calcualte();
+                    break;
+                case 9:
+                    PowerOfTwo.CheckPower();
+                    break;
+                default:
+                    break;
             }
-    }
+        }
+    } 
 }
