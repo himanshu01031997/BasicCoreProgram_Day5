5a43be72f9d0451d6d1984a4d44a6b8f6b70e09a
himanshu01031997 <hs010397@gmail.com>
2022-10-07 13:22:20 +0530

    add: add swap of numbers

---
+++ BasicCoreProgram/SwapNo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public static class SwapNo
+    {
+        public static void SwappingTwoNo()
+        {
+            int a, b;
+            Console.WriteLine("before swapping ");
+            Console.WriteLine("please enter first no :");
+            a=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please enter second no:");
+            b = Convert.ToInt32(Console.ReadLine());
+            a = a * b;
+            b = a / b;
+            a = a / b;
+            Console.WriteLine("after swapping numbers are :\n a is {0} \n b is {1}",a,b);
+
+        }
+    }
+}
